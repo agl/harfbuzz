616e2d40acb30aa8dfa4e839128247b61d327c71
Simon Hausmann <shausman@rhea.troll.no>
2006-12-15 14:30:50 +0100

    make it possible to run the generator just from the commandline without gui

--- test/qt/generator/main.cpp
+++ test/qt/generator/main.cpp
@@ -27,6 +27,49 @@
 #include <qtextlayout.h>
 #undef private
 
+static QString generate(const QString &family, int pixelSize, const QString &sampleText)
+{
+    QFont font;
+    font.setFamily(family);
+    font.setPixelSize(pixelSize);
+    font.setStyleStrategy(QFont::NoFontMerging);
+    font.setKerning(false);
+
+    QTextLayout layout(sampleText, font);
+    QTextEngine *e = layout.d;
+    e->itemize();
+    e->shape(0);
+
+    QString result;
+    QFontEngine *fe = e->fontEngine(e->layoutData->items[0]);
+    result = "# Using font '" + fe->fontDef.family + "'\n";
+
+    {
+        const QByteArray headTable = fe->getSfntTable(FT_MAKE_TAG('h', 'e', 'a', 'd'));
+        const quint32 revision = qFromBigEndian<quint32>(reinterpret_cast<const uchar *>(headTable.constData()) + 4);
+        result += "# Font Revision: " + QString::number(revision) + "\n";
+    }
+
+    result += "# Text: " + sampleText;
+    result += "\n# Codepoints: ";
+    for (int i = 0; i < sampleText.length(); ++i) 
+        result += "0x" + QString::number(sampleText.at(i).unicode(), 16) + " ";
+    result += "\n# Glyphs [glyph] [x advance] [x offset] [y offset]\n";
+    for (int i = 0; i < e->layoutData->items[0].num_glyphs; ++i) {
+        QGlyphLayout *g = e->layoutData->glyphPtr + i;
+        result += "0x" + QString::number(g->glyph, 16);
+        result += ' ';
+        result += QString::number(g->advance.x.toReal());
+        result += ' ';
+        result += QString::number(g->offset.x.toReal());
+        result += ' ';
+        result += QString::number(g->offset.y.toReal());
+        result += '\n';
+    }
+
+    return result;
+}
+
 class Generator : public QWidget, public Ui_Generator
 {
     Q_OBJECT
@@ -98,51 +141,81 @@ void Generator::on_sample_editingFinished()
     }
     glyphOutput->clear();
 
-    QFont font;
-    font.setFamily(QFontDatabase::applicationFontFamilies(fontId).first());
-    font.setPixelSize(12);
-    font.setStyleStrategy(QFont::NoFontMerging);
-    font.setKerning(false);
-
-    const QString str = sample->text();
-    QTextLayout layout(str, font);
-    QTextEngine *e = layout.d;
-    e->itemize();
-    e->shape(0);
-
-    QString result;
-    QFontEngine *fe = e->fontEngine(e->layoutData->items[0]);
-    result = "# Using font '" + fe->fontDef.family + "'\n";
-
-    {
-        const QByteArray headTable = fe->getSfntTable(FT_MAKE_TAG('h', 'e', 'a', 'd'));
-        const quint32 revision = qFromBigEndian<quint32>(reinterpret_cast<const uchar *>(headTable.constData()) + 4);
-        result += "# Font Revision: " + QString::number(revision) + "\n";
-    }
-
-    result += "# Text: ";
-    for (int i = 0; i < str.length(); ++i) 
-        result += "0x" + QString::number(str.at(i).unicode(), 16) + " ";
-    result += "\n# Glyphs [glyph] [x advance] [y advance] [x offset] [y offset]\n";
-    for (int i = 0; i < e->layoutData->items[0].num_glyphs; ++i) {
-        QGlyphLayout *g = e->layoutData->glyphPtr + i;
-        result += "0x" + QString::number(g->glyph, 16);
-        result += ' ';
-        result += QString::number(g->advance.x.toReal());
-        result += ' ';
-        result += QString::number(g->advance.y.toReal());
-        result += ' ';
-        result += QString::number(g->offset.x.toReal());
-        result += ' ';
-        result += QString::number(g->offset.y.toReal());
-        result += '\n';
-    }
-
+    QString result = generate(QFontDatabase::applicationFontFamilies(fontId).first(),
+                              /*pixelSize*/ 12,
+                              sample->text());
     glyphOutput->setPlainText(result);
+}
+
+static void help()
+{
+    printf("usage: generator /path/to/truetype/font.ttf pixelsize /path/to/sample-text-file.txt\n");
+    exit(1);
+}
+
+static int mainWithoutGui(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    QStringList args = app.arguments();
+
+    QString fontFile = args.at(1);
+    if (!QFile::exists(fontFile)) {
+        printf("font file %s does not exist.\n", qPrintable(fontFile));
+        return 1;
+    }
+    bool ok = false;
+    int pixelSize =  args.at(2).toInt(&ok);
+    if (!ok) {
+        printf("invalid pixel size: %s", qPrintable(args.at(2)));
+        return 1;
+    }
+
+    QString sampleFile = args.at(3);
+    if (!QFile::exists(sampleFile)) {
+        printf("sample file %s does not exist.\n", qPrintable(sampleFile));
+        return 1;
+    }
+
+    int id = QFontDatabase::addApplicationFont(fontFile);
+    if (id == -1) {
+        printf("cannot use font file %s\n", qPrintable(fontFile));
+        return 1;
+    }
+    QStringList families = QFontDatabase::applicationFontFamilies(id);
+    if (families.count() != 1) {
+        printf("wrong number of families in font file %s: %d -- should be 1\n", qPrintable(fontFile), families.count());
+        return 1;
+    }
+
+    QFile f(sampleFile);
+    if (!f.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        printf("cannot open sample file %s for reading\n", qPrintable(sampleFile));
+        return 1;
+    }
+
+    QString sampleText = QString::fromUtf8(f.readAll());
+    if (sampleText.endsWith(QLatin1Char('\r')))
+        sampleText.chop(1);
+    if (sampleText.endsWith(QLatin1Char('\n')))
+        sampleText.chop(1);
+
+    QString result = generate(families.first(), pixelSize, sampleText);
+
+    puts(result.toUtf8().constData());
+
+    return 0;
 }
 
 int main(int argc, char **argv)
 {
+    if (argc == 4)
+        return mainWithoutGui(argc, argv);
+    else if (argc != 1) {
+        help();
+        return 1;
+    }
+
     QApplication app(argc, argv);
 
     Generator w;
